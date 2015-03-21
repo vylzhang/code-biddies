cf69b59ed741af6486f9418c5cab7639de26d648
hingchow94 <hingchow94@gmail.com>
2015-03-20 23:21:12 -0400

    adding stock data structure

--- clientpy3.py
+++ clientpy3.py
@@ -1,11 +1,11 @@
 import socket
 import sys
-    
+
+maxMarketHistorySize = 50    
 
 def run(user, password, *commands):
     HOST, PORT = "codebb.cloudapp.net", 17429
 
-    
     data=user + " " + password + "\n" + "\n".join(commands) + "\nCLOSE_CONNECTION\n"
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
@@ -32,9 +32,38 @@ def subscribe(user, password):
         while rline:
             print(rline.strip())
             rline = sfile.readline()
+            
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Enter username, password, command")
+    else:
+        arg = sys.argv
+        run(arg[1],arg[2],arg[3])
 
 ## HING
+class stock:
 
+    def __init__(self):
+        self.ticker = ABC
+        self.netWorth = 123
+        self.bid = 123
+        self.ask = 456
+        self.dividendRatio = 0.1
+        self.volitility = 0.2
+        self.marketHistory = list() #market value
+        
+    def updateValues(self, ticker, netWorth, bid, ask, dividendRatio, volitility, currMarketValue):
+        self.ticker = ticker
+        self.netWorth = netWorth
+        self.bid = bid
+        self.ask = ask
+        self.dividendRatio = dividendRatio
+        self.volitility = volitility
+        if len(self.marketHistory) >= 50:
+            marketHistory.pop(0)
+        marketHistory.append(marketHistory)
+            
+        
 
 ## LEIGHTON
 
